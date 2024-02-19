78d11733035c8c64a792325ebc015e139eb8436f
shehzad-lalani <shehzad-lalani>
2024-02-19 09:58:51 +0000

    new file added

---
+++ My folder/(Clone) Demo Workbook.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+print("Hello Databricks!")
+
+# COMMAND ----------
+
+SELECT "Hello"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Title 1
+# MAGIC ## Title 2
+# MAGIC ### Title 3
+# MAGIC  
+
+# COMMAND ----------
+
+
